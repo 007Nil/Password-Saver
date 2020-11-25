f532ce2eda23b3299f53b5b1906a68475b8914ba
007Nil <sagniksarkar@ymail.com>
2020-11-26 00:25:28 +0530

    working on from submit js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,3 +1,8 @@
 $(function() {
-    console.log( "ready!" );
+    // console.log( "ready!" );
+
+    $("#register_from").on("submit", function(event){
+        event.preventDefault();
+        alert("STOP SUBMIT")
+    })
 });
