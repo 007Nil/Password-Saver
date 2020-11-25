afcd09a52cf39e85a2e91560786ff22a7269158f
007Nil <sagniksarkar@ymail.com>
2020-11-26 00:09:20 +0530

    Create registration page

---
+++ public/assets/js/connection.js
@@ -0,0 +1,17 @@
+// const {
+//     Sequelize
+// } = require('sequelize');
+
+
+// // Option 2: Passing parameters separately (other dialects)
+// const sequelize = new Sequelize('Password_Saver', 'root', 'P@ssw0rd', {
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
+
+// try {
+//     sequelize.authenticate();
+//     console.log('Connection has been established successfully.');
+// } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+// }
