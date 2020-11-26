41c68a7aef4e0cab9a7992ae3cb7a44b360cd51f
007Nil <sagniksarkar@ymail.com>
2020-11-26 22:57:37 +0530

    Minor changes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,8 +1,22 @@
-$(function() {
-    // console.log( "ready!" );
+require('mysql');
 
+$(function() {
     $("#register_from").on("submit", function(event){
         event.preventDefault();
-        alert("STOP SUBMIT")
+        // Get all data
+
+        let username = $("#username").val();
+        let email = $("#email").val();
+        let password = $("#password").val();
+        let conf_password = $("#repassword").val();
+        let phoneno = $("#phone_no").val();
+
+
+        if (password === conf_password){
+            console.log("submit");
+        }else{
+            console.log("error");
+        }
+
     })
 });
